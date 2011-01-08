1526f38efe587c170ed4cc06d3a9c7f71f6049fa
Mirko Kiefer <mirko.kiefer@arcor.de>
2011-01-08 20:13:58 +0200

    language recognizes more advanced model definitions

--- ometa/_attachments/modeling.js
+++ ometa/_attachments/modeling.js
@@ -1,7 +1,8 @@
-ometa Test <: Parser {
-  eval = thingDef:x -> x,
-  thingDef = thingHead:head  '\n' thingBody:body -> {body.label = head.name; body},
-  thingHead = "thing" spaces thingName:x -> {parent: '', name: x},
+ometa ModelParser <: Parser {
+  eval = thingDef*:x -> x,
+  thingDef = thingHead:head  '\n' thingBody:body '\n'* -> {label: head.name, type: head.type, properties: body},
+  thingHead = thingName:name spaces '(' thingName:type ')' -> {type: type, name: name} |
+              thingName:name -> {type: 'Thing', name: name},
   thingName = spaces firstAndRest(`upper, `letterOrDigit):name -> name.join(''),
   propertyName = spaces firstAndRest(`lower, `letterOrDigit):name -> name.join(''),
   propertyValue = variableValue:x -> x |
@@ -11,18 +12,27 @@ ometa Test <: Parser {
   stringValue = "'" letterOrDigit*:x "'" -> {type: 'string', value: x.join('')},
   numberValue = digit*:x -> {type: 'number', value: x.join('')},
   text = string*:x -> x.join(''),
-  thingBody = propertyDef*:props -> {var propObj = {}; props.forEach(function(each) {propObj[each.property] = each.propValue}); propObj},
-  propertyDef = propertyName:prop spaces ":" spaces propertyValue:value '\n' -> {property: prop, propValue: value}
+  thingBody = propertyDef*:props -> props,
+  propertyDef = propertyName:prop spaces ":" spaces propertyValue:value '\n' -> {propName: prop, propValue: value}
 }
 
-var test = "
-  thing Person
-    owns: Furniture
-    knows: Person
-    worksAt: Company
-    description: 'sometext'
-    someNumber: 8
-";
+var ModelTranslator = function() {
+  var translate = function(data) {
+    var code = 'var Thing = thingStore.lookup("uri:thing"); \n\n';
+    code += data.map(function(thingData) {
+      var label = thingData.label;
+      var type = thingData.type;
+      var thingCode = 'var ' + label + ' = ' + type + '.make([ \n';
+      thingCode += thingData.properties.map(function(prop) {
+        return '  [' + JSON.stringify(prop.propName) + ', ' + JSON.stringify(prop.propValue.value) + ']'
+      }).join(', \n');
+      thingCode += '\n]); \n';
+      return thingCode;
+    }).join('\n');
+    return code;
+  };
+  return {translate: translate}
+}
 
 var testAdvanced = "
   Person(Thing)
@@ -37,7 +47,12 @@ var testAdvanced = "
     knows: Graham
   
   Graham(Person)
-    owns: Table
+    owns: Table 
+    
+  EmptyThing
+  AnotherThing
 ";
-var translation = Test.matchAll(test, 'eval');
-JSON.stringify(translation);
+var parsedModel = ModelParser.matchAll(testAdvanced, 'eval');
+console.log(parsedModel);
+ModelTranslator().translate(parsedModel);
+//ModelCompiler.matchAll(translation, 'eval');
