342fe85e90543457d64c9f530e8727f920a0834e
Mirko Kiefer <mirko.kiefer@arcor.de>
2011-01-08 23:30:09 +0200

    language now keeps track of already defined things

--- ometa/_attachments/modeling.js
+++ ometa/_attachments/modeling.js
@@ -4,29 +4,46 @@ ometa ModelParser <: Parser {
   thingHead = thingName:name spaces '(' thingName:type ')' -> {type: type, name: name} |
               thingName:name -> {type: 'Thing', name: name},
   thingName = spaces firstAndRest(`upper, `letterOrDigit):name -> name.join(''),
+  thingBody = propertyDef*:props -> props,
+  propertyDef = propertyName:prop spaces ":" spaces propertyValue:value '\n' -> {propName: prop, propValue: value},
   propertyName = spaces firstAndRest(`lower, `letterOrDigit):name -> name.join(''),
   propertyValue = variableValue:x -> x |
                   stringValue:x -> x |
                   numberValue:x -> x,
   variableValue = thingName:x -> {type: 'thing', value: x},
   stringValue = "'" letterOrDigit*:x "'" -> {type: 'string', value: x.join('')},
-  numberValue = digit*:x -> {type: 'number', value: x.join('')},
-  text = string*:x -> x.join(''),
-  thingBody = propertyDef*:props -> props,
-  propertyDef = propertyName:prop spaces ":" spaces propertyValue:value '\n' -> {propName: prop, propValue: value}
+  numberValue = digit*:x -> {type: 'number', value: parseFloat(x.join(''))}
 }
 
 var ModelTranslator = function() {
   var translate = function(data) {
-    var code = 'var Thing = thingStore.lookup("uri:thing"); \n\n';
+    var code = 'var Thing = thingStore.lookup("uri:thing");\n';
+    code += 'var labelProp = thingStore.lookup("uri:thing/property/label");\n';
+    var thingsInScope = ['Thing'];
     code += data.map(function(thingData) {
       var label = thingData.label;
       var type = thingData.type;
-      var thingCode = 'var ' + label + ' = ' + type + '.make([ \n';
-      thingCode += thingData.properties.map(function(prop) {
-        return '  [' + JSON.stringify(prop.propName) + ', ' + JSON.stringify(prop.propValue.value) + ']'
-      }).join(', \n');
-      thingCode += '\n]); \n';
+      var thingCode = '';
+      var properties = thingData.properties.map(function(prop) {
+        if(prop.propValue.type == 'thing') {
+          var propValue = prop.propValue.value;
+        } else {
+          var propValue = JSON.stringify(prop.propValue.value);
+        }
+        if(thingsInScope.indexOf(prop.propName) > -1) {
+          var propertyName = prop.propName;
+        } else {
+          var propertyName = JSON.stringify(prop.propName);
+        }
+        return '  [' + propertyName + ',' + propValue + ']'
+      });
+      if(thingsInScope.indexOf(label) > -1) {
+        thingCode = label + '.extend([ \n' + properties.join(',\n') + '\n]);\n';
+      } else {
+        thingsInScope.push(label);
+        properties.push('  [labelProp,' + JSON.stringify(label) + ']');
+        thingCode = 'var ' + label + ' = ' + type + '.make([ \n' + properties.join(',\n') + '\n]);\n';
+      }
       return thingCode;
     }).join('\n');
     return code;
@@ -35,10 +52,12 @@ var ModelTranslator = function() {
 }
 
 var testAdvanced = "
+
+  Graham
+  
   Person(Thing)
     owns: Furniture
     knows: Person
-    worksAt: Company
     description: 'sometext'
     someNumber: 8
   
@@ -47,12 +66,8 @@ var testAdvanced = "
     knows: Graham
   
   Graham(Person)
-    owns: Table 
-    
-  EmptyThing
-  AnotherThing
+    owns: Table
 ";
 var parsedModel = ModelParser.matchAll(testAdvanced, 'eval');
 console.log(parsedModel);
 ModelTranslator().translate(parsedModel);
-//ModelCompiler.matchAll(translation, 'eval');
